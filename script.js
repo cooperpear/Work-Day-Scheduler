dc92fae4658c1454ed038d0365bd6dbd6a92d01d
Cooper Pearson <mptprogesterone@gmail.com>
2019-11-07 18:35:09 -0600

    cleaned script

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(time, value)
 })
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 function hourupdater() {
     var currenthour = moment().hours()
@@ -26,5 +34,5 @@ function hourupdater() {
 
 hourupdater();
 
-var interval = setInterval(hourUpdater, 15000);
+var interval = setInterval(hourupdater, 15000);
 
